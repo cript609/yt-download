c4b393efd5271c7a0b85fb2fd7d3936c8847ab3f
cript609 <43116559+cript609@users.noreply.github.com>
2018-09-09 19:31:39 +0200

    Add files via upload

---
+++ yt_download/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+#from .forms import Downloader
+from pytube import YouTube
+from .settings import BASE_DIR
+
+
+def home_page(request):
+    if request.method == 'POST':
+        x = request.POST['youtube_url']
+        if request.POST['youtube_url'] == '':
+            return render(request, 'home_page.html', context={'vid_download': ''})
+        yt = YouTube(x)
+        stream = yt.streams.first()
+        x_download = stream.download(BASE_DIR + '\downloads')
+        vid_download = BASE_DIR + '\downloads\\' + stream.default_filename
+        contex = {
+            'vid_download': vid_download
+        }
+        return render(request, 'home_page.html', contex)
+
+    return render(request, 'home_page.html')
